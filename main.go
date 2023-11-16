cb3cd0b9fd29235c3f5974aab33ec65b68487a41
suhaibmtz <suhaibmtz68@gmail.com>
2023-11-16 18:08:50 +0200

    test

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 var mytemplate *template.Template
 
-const Version = "0.625 15Nov"
+const Version = "0.6.25 16Nov"
 
 func main() {
 	mytemplate = template.Must(template.ParseGlob("templates/*.html"))
